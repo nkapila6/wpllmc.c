02e46fe6a188b50c711035640e1d7c81790d65be
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2026-02-22 19:41:19 +0400

    feat: updated logger

--- logger.c
+++ logger.c
@@ -1,4 +1,5 @@
 #include "logger.h"
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,6 +9,11 @@
 // https://stackoverflow.com/questions/6508461/logging-library-for-c
 
 LOGLEVEL current_log_level = LOG_WARN;
+
+#define CLR_RED "\x1b[31m"
+#define CLR_GREEN "\x1b[32m"
+#define CLR_YELLOW "\x1b[33m"
+#define CLR_RESET "\x1b[0m"
 
 const char *level_to_str(LOGLEVEL level) {
   switch (level) {
@@ -22,20 +28,47 @@ const char *level_to_str(LOGLEVEL level) {
   }
 }
 
-void logger(LOGLEVEL level, const char *tag, const char *msg) {
+void logger(LOGLEVEL level, const char *tag, const char *fmt, ...) {
   // only print if lvl is equal or higher than setting
   if (level < current_log_level) {
     return;
   }
 
+  char msg[1024];
+  va_list args;
+  va_start(args, fmt);
+  vsnprintf(msg, sizeof(msg), fmt, args);
+  va_end(args);
+
   time_t now;
   time(&now);
-
   char *date = ctime(&now);
   if (date)
     date[strlen(date) - 1] = '\0';
 
-  fprintf(stderr, "%s [%s]: %s\n", date, tag, msg);
+  const char *color;
+  const char *level_str;
+  switch (level) {
+  case LOG_INFO:
+    color = CLR_GREEN;
+    level_str = "INFO";
+    break;
+  case LOG_WARN:
+    color = CLR_YELLOW;
+    level_str = "WARN";
+    break;
+  case LOG_ERROR:
+    color = CLR_RED;
+    level_str = "ERROR";
+    break;
+  default:
+    color = CLR_RESET;
+    level_str = "LOG";
+    break;
+  }
+
+  fprintf(stderr, "%s %s [%s] [%s]%s: %s\n", color, date, level_str, tag,
+          CLR_RESET, msg);
 }
 
 void logger_to_file(const char *tag, const char *message) {
--- logger.h
+++ logger.h
@@ -5,6 +5,6 @@ typedef enum { LOG_INFO, LOG_WARN, LOG_ERROR } LOGLEVEL;
 
 extern LOGLEVEL current_log_level;
 
-void logger(LOGLEVEL level, const char *tag, const char *msg);
+void logger(LOGLEVEL level, const char *tag, const char *fmt, ...);
 
 #endif
