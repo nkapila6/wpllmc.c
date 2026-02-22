ce183d5f2df478607b32455240156aa32cad865b
Nikhil Kapila <12816113+nkapila6@users.noreply.github.com>
2026-02-22 19:28:06 +0400

    chore: changed main to reflect proj name

--- wpllm.c
+++ wpllm.c
@@ -1,4 +1,6 @@
+#include "logger.h"
 #include <curl/curl.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,7 +11,11 @@
   "options:\n"                                                                 \
   " -u [url]  The wordpress site URL to prepare llms.txt (website examples: "  \
   "https://wordpress.org/showcase)\n"                                          \
-  " -h        Show this help message\n"
+  " -p        Boolean flag to get only pages.\n"                               \
+  " -b        Boolean flag to get only (blog) posts.\n"                        \
+  " -h        Show this help message.\n"                                       \
+  " -v        Verbose boolean flag, off (quiet) by default.\n"                 \
+  "If p and b are not passed, both pages and posts are considered.\n"
 
 int is_url_valid(const char *url) {
   CURLU *u = curl_url();
@@ -21,30 +27,47 @@ int is_url_valid(const char *url) {
 int main(int argc, char *argv[]) {
   int opt;
   char *url = NULL;
+  bool pages = false, blogs = false;
 
   if (argc < 2) {
     fprintf(stderr, "%s", USAGE);
     return EXIT_SUCCESS;
   }
 
-  printf("Welcome to wpllm.c!\n");
-  while ((opt = getopt(argc, argv, "hu:")) != -1) {
+  // https://www.man7.org/linux/man-pages/man3/getopt.3.html
+  while ((opt = getopt(argc, argv, "hvpbu:")) != -1) {
     switch (opt) {
     case 'u':
       url = optarg;
       break;
-    default:
+
+    case 'p':
+      pages = true;
+      break;
+
+    case 'b':
+      blogs = true;
+      break;
+
+    case 'v':
+      current_log_level = LOG_INFO;
+      break;
+
+    default: // considering h will fall here too.
       fprintf(stderr, "%s", USAGE);
       return EXIT_SUCCESS;
     }
   }
 
   if ((url == NULL) || (is_url_valid(url) == 0)) {
-
     fprintf(stderr, "The URL %s is invalid, kindly follow usage below:\n%s",
             url, USAGE);
     return EXIT_FAILURE;
   }
 
+  printf("Welcome to wpllm.c!\n");
+  // logger();
   printf("\n entered: %s", url);
+  printf("\nvalue of pages: %d", pages);
+  printf("\nvalue of blog posts: %d", blogs);
 }
